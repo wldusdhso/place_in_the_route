aa9c16fcd08dba9cefa7e13e4a0884970ff61255
JeongJiyeon <wldusdhso@gmail.com>
2020-05-30 17:10:28 +0900

    http_host header 수정

--- ejproject/ejproject/settings.py
+++ ejproject/ejproject/settings.py
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
