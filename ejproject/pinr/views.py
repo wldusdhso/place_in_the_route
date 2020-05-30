7bdffde23a89386d2d87dcab02195e12966077fb
choieunsol <achldmsthf@kookmin.ac.kr>
2020-05-30 18:13:42 +0900

    error

--- ejproject/pinr/views.py
+++ ejproject/pinr/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
-def main(request):
-    return render(request, 'main.html')
+
+def index1(request):
+    return render(request, 'index1.html')
+
+def index2(request):
+    return render(request, 'index2.html')
+
+def side1(request):
+    return render(request, 'side1.html')
+
+def side2(request):
+    return render(request, 'side2.html')
+
