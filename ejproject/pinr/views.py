660765e7deaa4e43155c3c422a3a9e3cbc9deab8
JeongJiyeon <wldusdhso@gmail.com>
2020-06-19 12:17:58 +0900

    trash

--- ejproject/pinr/views.py
+++ ejproject/pinr/views.py
@@ -4,10 +4,25 @@ from django.shortcuts import render
 def base(request):
     return render(request, 'base.html')
 
-def find_route(request):
-    return render(request, 'find_route.html')
-
 def find_place(request):
-    return render(request, 'find_place.html')
+    # sx = request.POST['sx']
+    # sy = request.POST['sy']
+    # ex = request.POST['ex']
+    # ey = request.POST['ey']
+    # route_id = request.POST['route_id']
+
+    sx = 0
+    sy = 0
+    ex = 0
+    ey = 0
+    route_id = 0
+
+    return render(request, 'find_place.html', {
+        'sx' : sx,
+        'sy' : sy,
+        'ex' : ex,
+        'ey' : ey,
+        'route_id' : route_id,
+        })
 
 
