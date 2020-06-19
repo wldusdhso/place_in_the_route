e2875e13bad236c141d0fa78b9b6908c8c8253fa
JeongJiyeon <wldusdhso@gmail.com>
2020-06-19 13:19:19 +0900

    trash

--- ejproject/pinr/views.py
+++ ejproject/pinr/views.py
@@ -11,10 +11,10 @@ def find_place(request):
     # ey = request.POST['ey']
     # route_id = request.POST['route_id']
 
-    sx = 0
-    sy = 0
-    ex = 0
-    ey = 0
+    sx = 126.9027279
+    sy = 37.5349277
+    ex = 126.9145430
+    ey = 37.5499421
     route_id = 0
 
     return render(request, 'find_place.html', {
