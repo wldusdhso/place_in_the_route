f6cba20dc95fae5fea4c15b7c9628511e4ebd742
choieunsol <achldmsthf@kookmin.ac.kr>
2020-06-21 17:52:55 +0900

    getRouteListItem

--- ejproject/pinr/static/js/base.js
+++ ejproject/pinr/static/js/base.js
@@ -246,7 +246,7 @@ function getRouteListItem(route) {
     itemStr += '  <small class="text-muted">' + route.info.payment + '원' + '</small>';
     console.log(route.subPath[0].trafficType);
     for (i = 0; route.subPath.length; i++) {
-        if (route.subPath[i].trafficType == 1) { // 지하철일 때
+        if (route.subPath[i].trafficType == '1') { // 지하철일 때
             itemStr += '   <h5 class="mb-1">' + route.subPath[i].lane[0].subwayCode + '호선 ' ;
             if (i == 0 && i != route.subPath.length) {
                 itemStr += route.subPath[i].startName + '승차' + '</h5>';
@@ -268,8 +268,6 @@ function getRouteListItem(route) {
             } else {
                 itemStr += route.subPath[i].startName + '환승' + '</h5>';
             }
-        } else {
-            
         }
     }
 
