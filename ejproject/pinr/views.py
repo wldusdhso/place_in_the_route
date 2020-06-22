81b2cf6fc2f54d1e8e165b2db6e5cb6f5e203dd7
JeongJiyeon <wldusdhso@gmail.com>
2020-06-22 16:11:39 +0900

    test

--- ejproject/pinr/views.py
+++ ejproject/pinr/views.py
@@ -5,18 +5,18 @@ def base(request):
     return render(request, 'base.html')
 
 def find_place(request, sx, sy, ex, ey, route):
-    sx = 126.9027279
-    sy = 37.5349277
-    ex = 126.9145430
-    ey = 37.5499421
-    route_id = 0
+    # sx = 126.9027279
+    # sy = 37.5349277
+    # ex = 126.9145430
+    # ey = 37.5499421
+    # route_id = 0
 
     return render(request, 'find_place.html', {
         'sx' : sx,
         'sy' : sy,
         'ex' : ex,
         'ey' : ey,
-        'route_id' : route_id,
+        'route' : route,
         })
 
 
